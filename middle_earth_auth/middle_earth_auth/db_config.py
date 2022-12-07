49e800c4bf1c3cb2d56a520388dccbc7102a3ebd
User <rothweil.miklos@gmail.com>
2022-12-07 21:13:47 +0100

    Remove custom USER and DB name from psotgresql, use default "postgres" for both (middle-earth_front, _auth, _items, _inventory)

--- middle_earth_auth/middle_earth_auth/db_config.py
+++ middle_earth_auth/middle_earth_auth/db_config.py
@@ -4,7 +4,7 @@ from . import secret_key
 
 
 def dev_or_prod_db(debug, base_directory):
-    if debug == 1:
+    if debug:
         SQLITE_DATABASE = {
             'ENGINE': 'django.db.backends.sqlite3',
             'NAME': base_directory / 'db.sqlite3',
@@ -12,19 +12,15 @@ def dev_or_prod_db(debug, base_directory):
         return SQLITE_DATABASE
     else:
         # for development uncomment the three lines below and comment the three lines below that
-        # POSTGRES_DB = os.environ.get('POSTGRES_DB')
-        # POSTGRES_USER = os.environ.get('POSTGRES_USER')
         # POSTGRES_PASSWORD = os.environ.get('POSTGRES_PASSWORD')
 
         # for production uncomment the three lines below, and comment the three lines above
-        POSTGRES_DB = secret_key.get_secret_value('auth_postgres_db')
-        POSTGRES_USER = secret_key.get_secret_value('auth_postgres_user')
         POSTGRES_PASSWORD = secret_key.get_secret_value('auth_postgres_password')
 
         POSTGRESQL_DATABASE = {
             'ENGINE': 'django.db.backends.postgresql_psycopg2',
-            'NAME': POSTGRES_DB,
-            'USER': POSTGRES_USER,
+            'NAME': 'postgres',
+            'USER': 'postgres',
             'PASSWORD': POSTGRES_PASSWORD,
             'HOST': 'db_auth',
             'PORT': '5432',
--- middle_earth_inventory/middle_earth_inventory/db_config.py
+++ middle_earth_inventory/middle_earth_inventory/db_config.py
@@ -5,7 +5,7 @@ from . import secret_key
 
 
 def dev_or_prod_db(debug, base_directory):
-    if debug == 1:
+    if debug:
         SQLITE_DATABASE = {
             'ENGINE': 'django.db.backends.sqlite3',
             'NAME': base_directory / 'db.sqlite3',
@@ -13,19 +13,15 @@ def dev_or_prod_db(debug, base_directory):
         return SQLITE_DATABASE
     else:
         # for development uncomment the three lines below and comment the three lines below that
-        # POSTGRES_DB = os.environ.get('POSTGRES_DB')
-        # POSTGRES_USER = os.environ.get('POSTGRES_USER')
         # POSTGRES_PASSWORD = os.environ.get('POSTGRES_PASSWORD')
 
         # for production uncomment the three lines below, and comment the three lines above
-        POSTGRES_DB = secret_key.get_secret_value('inventory_postgres_db')
-        POSTGRES_USER = secret_key.get_secret_value('inventory_postgres_user')
         POSTGRES_PASSWORD = secret_key.get_secret_value('inventory_postgres_password')
 
         POSTGRESQL_DATABASE = {
-            'ENGINE': 'django.db.backends.postgresql_psycopg2',
-            'NAME': POSTGRES_DB,
-            'USER': POSTGRES_USER,
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'postgres',
+            'USER': 'postgres',
             'PASSWORD': POSTGRES_PASSWORD,
             'HOST': 'db_inventory',
             'PORT': '5432',
