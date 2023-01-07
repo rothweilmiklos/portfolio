7a36ea84a4c14c64ff350e1340badfdbb2a51bc9
User <rothweil.miklos@gmail.com>
2023-01-07 23:18:13 +0100

    Create view for randomapis service

--- task_queue/random_apis/main/views.py
+++ task_queue/random_apis/main/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views import View
+from .celery_task import send_tasks
 
 
 # Create your views here.
 
-class APIView(TemplateView):
-    pass
+class APIView(View):
+
+    @staticmethod
+    def get(request):
+        return render(request, 'main/random_apis.html', context={"api": False})
+
+    @staticmethod
+    def post(request):
+        api_results = send_tasks()
+        return render(request, 'main/random_apis.html', context={"api": api_results})
